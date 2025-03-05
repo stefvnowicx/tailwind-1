e0d643ea2c7770944506fa215f4a88d60b52aaa5
Mateusz <stefvnowicx@gmail.com>
2025-03-05 17:53:24 +0100

    first solo project tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+   content: [
+      "./index.html", // Określ ścieżki do swoich plików HTML i JS
+   ],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
+};
+
+// npm run build/watch
